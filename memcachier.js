e25edb05cf04e636986faad1f13c8757f4ecd0b7
Nguyen Tien Thanh <tienthanhit97@gmail.com>
2020-11-29 09:09:20 +0700

    first commit

---
+++ memcachier.js
@@ -0,0 +1,13 @@
+var memjs = require('memjs')
+
+var mc = memjs.Client.create(process.env.MEMCACHIER_SERVERS, {
+    failover: true,  // default: false
+    timeout: 1,      // default: 0.5 (seconds)
+    keepAlive: true  // default: false
+});
+
+module.exports = mc;
+
+
+
+
---
+++ routes/routes.js
@@ -0,0 +1,37 @@
+
+const mc = require('../memcachier');
+
+const appRouter = (app, fs) => {
+    // we've added in a default route here that handles empty routes
+    // at the base API url
+    app.get('/', (req, res) => {
+        mc.get(req.query.roomId, function(err, val) {
+            if(err != null) {
+                res.send(' Error');
+            }
+            else {
+                if (val != null) {
+                    res.send(val);
+                } else {
+                    res.send('not found');
+                }
+            }
+        })
+    });
+
+    app.post('/', (req, res) => {
+
+        mc.set(req.body.roomId, JSON.stringify(req.body.data), {expires:0}, function(err, val) {
+            if(err != null) {
+                console.log('Error setting value: ' + err);
+                res.send('Error setting value');
+            } else {
+                res.send('Success');
+            }
+        });
+    });
+
+};
+
+// this line is unchanged
+module.exports = appRouter;
